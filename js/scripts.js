61c64f970e53a1ea4dbb6ed55bcf053db0e82555
aowsr <35462172+aowsr@users.noreply.github.com>
2021-03-29 17:55:52 +0100

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
+var menuBtn = document.querySelector(".menu-btn");
+var navItems = document.querySelector(".nav_background");
+var menuOpen = false;
+
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
+    navItems.classList.remove("hidden");
     menuOpen = true;
   } else {
     menuBtn.classList.remove("open");
+    navItems.classList.add("hidden");
     menuOpen = false;
   }
 });
